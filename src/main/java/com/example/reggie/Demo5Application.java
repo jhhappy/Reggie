47badb9079ebbfc68e54f443a2334bfa46dd0849
jjh <jh18438000868@qq.com>
2023-06-02 18:54:44 +0800

    Redis缓存套餐数据

--- src/main/java/com/example/reggie/Demo5Application.java
+++ src/main/java/com/example/reggie/Demo5Application.java
@@ -4,10 +4,12 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.cache.annotation.EnableCaching;
 
 @SpringBootApplication
 @Slf4j
 @ServletComponentScan
+@EnableCaching//开启缓存
 public class Demo5Application {
 
     public static void main(String[] args) {
--- src/main/java/com/example/reggie/controller/SetmealController.java
+++ src/main/java/com/example/reggie/controller/SetmealController.java
@@ -16,6 +16,8 @@ import com.example.reggie.service.SetmealService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -45,6 +47,7 @@ public class SetmealController {
      * @return
      */
     @PostMapping
+ //   @CacheEvict(value = "setmealCache",allEntries = true) //清理setmealCache缓存中的所有数据
     public R<String> save(@RequestBody SetmealDto setmealDto){
         log.info("套餐信息；{}",setmealDto);
         setmealService.saveWithDish(setmealDto);
@@ -57,7 +60,6 @@ public class SetmealController {
     public R<Page> page(int page,int pageSize,String name){
         Page<Setmeal> pageInfo=new Page<>(page,pageSize);
         Page<SetmealDto> dtoPage=new Page<>(page,pageSize);
-
         LambdaQueryWrapper<Setmeal> queryWrapper=new LambdaQueryWrapper<>();
         queryWrapper.like(name!=null,Setmeal::getName,name);
         queryWrapper.orderByDesc(Setmeal::getUpdateTime);
@@ -89,6 +91,7 @@ public class SetmealController {
      * @return
      */
     @DeleteMapping
+    @CacheEvict(value = "setmealCache",allEntries = true) //清理setmealCache缓存中的所有数据
     public R<String> delete(@RequestParam List<Long> ids){
         log.info("ids:{}",ids);
         setmealService.removeWithDish(ids);
@@ -106,6 +109,7 @@ public class SetmealController {
      * @return
      */
     @GetMapping("/list")
+    @Cacheable(value = "setmealCache",key="#setmeal.categoryId+'_'+#setmeal.status")
     public R<List<Setmeal>> list( Setmeal setmeal){
         LambdaQueryWrapper<Setmeal> queryWrapper=new LambdaQueryWrapper<>();
         queryWrapper.eq(setmeal.getCategoryId()!=null,Setmeal::getCategoryId,setmeal.getCategoryId());
